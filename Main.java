8248591c9274ab55e566dfd5f2687b7102ba16b0
JiaruFu <43692362+JiaruFu@users.noreply.github.com>
2019-04-21 19:17:35 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -21,26 +22,34 @@ public final class Main extends Application {
 
   @Override
   public void start(Stage stage) {  
-    GridPane inputGridPane = new GridPane();
+    BorderPane borderPane = new BorderPane();
+    GridPane gridPane1=new GridPane();
+    GridPane gridPane2=new GridPane();
+    GridPane gridPane3=new GridPane();
     stage.setTitle("Quiz Generator");
     FileChooser fileChooser = new FileChooser();
     Button openButton = new Button("Load Question");
     Label label=new Label("Quiz Generator");
- Button nextButton = new Button("Next");  
-    GridPane.setRowIndex(label, 0);
+    Button operateQuiz = new Button("operate quiz"); 
+    Button addQuestion = new Button("add question"); 
     
+    Button loadData = new Button("Load Data"); 
     
-    //Setting the vertical and horizontal gaps between the columns 
-    inputGridPane.setVgap(100); 
-    inputGridPane.setHgap(50);       
+   //top
+    gridPane1.setAlignment(Pos.CENTER); 
+    gridPane1.getChildren().add(label);
+    borderPane.setTop(gridPane1);
     
-    //Setting the Grid alignment 
-    inputGridPane.setAlignment(Pos.CENTER); 
-     
-    //Arranging all the nodes in the grid 
-    inputGridPane.add(label, 1, 0); 
-    inputGridPane.add(openButton, 2, 2); 
-    inputGridPane.add(nextButton, 0, 2); 
+    //bottom
+    gridPane2.add(operateQuiz, 0,0);
+    gridPane2.add(addQuestion, 1,0);
+    GridPane.setMargin(operateQuiz, new Insets(10, 10, 10, 80)); 
+    GridPane.setMargin(addQuestion, new Insets(10, 10, 10, 110)); 
+    borderPane.setBottom(gridPane2);
+    
+    //left TODO might need a gridPane...
+  //  gridPane3.
+    borderPane.setLeft(gridPane3);
 
     openButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
@@ -53,11 +62,10 @@ public final class Main extends Application {
       }
     });  
    
-    inputGridPane.setPadding(new Insets(12, 12, 12, 12));
-    GridPane.setMargin(nextButton, new Insets(0, 0, 0,120));
- 
+    borderPane.setPadding(new Insets(12, 12, 12, 12));
+  
     
-    nextButton.setOnAction(new EventHandler<ActionEvent>() {
+    loadData.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(final ActionEvent e) {
         File file = fileChooser.showOpenDialog(stage);
@@ -69,7 +77,7 @@ public final class Main extends Application {
     });
     
 
-    stage.setScene(new Scene(inputGridPane, 500, 500));
+    stage.setScene(new Scene(borderPane, 500, 500));
     stage.show();
   }
 
