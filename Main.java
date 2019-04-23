34ff9986ddbf45624cce5c819c4d0412d85ef69e
JiaruFu <43692362+JiaruFu@users.noreply.github.com>
2019-04-23 15:51:25 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -38,35 +38,42 @@ public final class Main extends Application {
     
    //top
     Label label=new Label("Quiz Generator");
-    label.setFont(Font.font("Cambria", 32));
+    label.setFont(Font.font("Cambria", 50));
     gridPane1.setAlignment(Pos.CENTER); 
     gridPane1.getChildren().add(label);
     borderPane.setTop(gridPane1);
     
     //bottom
     Button operateQuiz = new Button("generate quiz"); 
-    Button addQuestion = new Button("add question"); 
+    operateQuiz.setStyle("-fx-font-size:15");
+    Button addQuestion = new Button("add question");
+    addQuestion.setStyle("-fx-font-size:15");
     gridPane2.add(operateQuiz, 0,0);
     gridPane2.add(addQuestion, 1,0);
-    GridPane.setMargin(operateQuiz, new Insets(10, 10, 10, 50)); 
-    GridPane.setMargin(addQuestion, new Insets(10, 10, 10, 80)); 
+    GridPane.setMargin(operateQuiz, new Insets(20, 10, 10, 100)); 
+    GridPane.setMargin(addQuestion, new Insets(20, 10, 10, 180)); 
     borderPane.setBottom(gridPane2);
     
     //left 
     Label numberQ=new Label("The number of questions: ");
+    numberQ.setFont(Font.font("Cambria", 32));
     GridPane.setMargin(numberQ, new Insets(10, 10, 10, 10)); 
     
     TextField textField=new TextField();
     textField.setMaxWidth(160);
+    textField.setMaxHeight(50);
     Label label2=new Label("/ 50");
+    label2.setFont(Font.font("Cambria", 32));
     Pane secondRow = new HBox(5);
     secondRow.getChildren().add(textField);
     secondRow.getChildren().add(label2);
     
     Button saveFile = new Button("Save  File"); 
-    saveFile.setMaxWidth(150);
+    saveFile.setMaxWidth(250);
+    saveFile.setStyle("-fx-font-size:15");
     Button loadQuestion = new Button("Load Question");
-    loadQuestion.setMaxWidth(150);
+    loadQuestion.setMaxWidth(250);
+    loadQuestion.setStyle("-fx-font-size:15");
     gridPane3.add(numberQ, 0,0);
     GridPane.setMargin(numberQ, new Insets(10, 10, 10, 40)); 
     gridPane3.add(secondRow, 0,1);
@@ -78,10 +85,10 @@ public final class Main extends Application {
     
     Image image = new Image("pig.png");
     ImageView imageview = new ImageView(image);
-    imageview.setFitHeight(160);
-    imageview.setFitWidth(160);
+    imageview.setFitHeight(170);
+    imageview.setFitWidth(170);
     gridPane3.add(imageview, 0,5);
-    GridPane.setMargin(imageview, new Insets(10, 10, 10, 40)); 
+    GridPane.setMargin(imageview, new Insets(10, 10, 10, 80)); 
     borderPane.setLeft(gridPane3);
     
     
@@ -92,8 +99,8 @@ public final class Main extends Application {
     ObservableList<String> items =FXCollections.observableArrayList (
         "topic1", "topic2", "topic3", "topic4","topic5");
     list.setItems(items);
-    list.setPrefHeight(100);
-    list.setPrefWidth(170);
+    list.setPrefHeight(70);
+    list.setPrefWidth(300);
     System.out.println(list.getSelectionModel());
     list.setOnMouseClicked(new EventHandler<Event>() {
       @Override
@@ -107,17 +114,7 @@ public final class Main extends Application {
     borderPane.setRight(list);
     
     FileChooser fileChooser = new FileChooser();
-    saveFile.setOnAction(new EventHandler<ActionEvent>() {
-      @Override
-      public void handle(final ActionEvent e) {
-        File file = fileChooser.showOpenDialog(stage);
-        if (file != null) {
-         //load data to where????? TODO
-          
-        }
-      }
-    });  
-  
+   
     loadQuestion.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(final ActionEvent e) {
